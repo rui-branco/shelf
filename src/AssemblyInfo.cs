385347b0423f55c4bed3821ec78dcf263a89f1cd
rui.branco <ruibranco.95@gmail.com>
2026-09-21 17:17:42 +0100

    chore(version): bump to 1.0.2

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // csc turns these into the Win32 version resource. Without them Windows has no
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Downloads stack for your desktop")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 [assembly: ComVisible(false)]
