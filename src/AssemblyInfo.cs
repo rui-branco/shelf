b3aaafa063a50657a4e7b7b2e8813a4bcd164689
rui.branco <ruibranco.95@gmail.com>
2026-09-21 17:57:14 +0100

    chore(version): bump to 1.0.3

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Downloads stack for your desktop")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 [assembly: ComVisible(false)]
